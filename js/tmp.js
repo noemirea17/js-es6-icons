1d641f703b39aa6251f6635dd42fc0a7e5b02ed5
Noemi <noemi.rea@libero.it>
2021-03-17 15:59:35 +0100

    milestone 1

---
+++ js/tmp.js
@@ -0,0 +1,116 @@
+$(document).ready(function () {
+  const icons = [
+    {
+      name: "cat",
+      prefix: "fa-",
+      type: "animal",
+      family: "fas",
+    },
+    {
+      name: "crow",
+      prefix: "fa-",
+      type: "animal",
+      family: "fas",
+    },
+    {
+      name: "dog",
+      prefix: "fa-",
+      type: "animal",
+      family: "fas",
+    },
+    {
+      name: "dove",
+      prefix: "fa-",
+      type: "animal",
+      family: "fas",
+    },
+    {
+      name: "dragon",
+      prefix: "fa-",
+      type: "animal",
+      family: "fas",
+    },
+    {
+      name: "horse",
+      prefix: "fa-",
+      type: "animal",
+      family: "fas",
+    },
+    {
+      name: "hippo",
+      prefix: "fa-",
+      type: "animal",
+      family: "fas",
+    },
+    {
+      name: "fish",
+      prefix: "fa-",
+      type: "animal",
+      family: "fas",
+    },
+    {
+      name: "carrot",
+      prefix: "fa-",
+      type: "vegetable",
+      family: "fas",
+    },
+    {
+      name: "apple-alt",
+      prefix: "fa-",
+      type: "vegetable",
+      family: "fas",
+    },
+    {
+      name: "lemon",
+      prefix: "fa-",
+      type: "vegetable",
+      family: "fas",
+    },
+    {
+      name: "pepper-hot",
+      prefix: "fa-",
+      type: "vegetable",
+      family: "fas",
+    },
+    {
+      name: "user-astronaut",
+      prefix: "fa-",
+      type: "user",
+      family: "fas",
+    },
+    {
+      name: "user-graduate",
+      prefix: "fa-",
+      type: "user",
+      family: "fas",
+    },
+    {
+      name: "user-ninja",
+      prefix: "fa-",
+      type: "user",
+      family: "fas",
+    },
+    {
+      name: "user-secret",
+      prefix: "fa-",
+      type: "user",
+      family: "fas",
+    },
+  ];
+
+  const square = $(".icons");
+
+  icons.forEach((element) => {
+    const { name, prefix, type, family } = element;
+
+    const onHtml = `
+	<div>
+	  	<i class="${family} ${prefix}${name}"></i>
+		<p>${name}</p>
+		  
+    </div>
+	  `;
+
+    square.append(onHtml);
+  });
+});
